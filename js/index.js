d225ede043ae59049babd6182aae5d5aa999ac05
osiriwat <osiriwat@gmail.com>
2017-08-27 19:35:41 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -58,19 +58,7 @@ var app = {
 
         scanner.scan( function (result) { 
 
-			$('#Load').load('http://www.google.co.in');
-			
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
+	  $('#Load').load('http://www.google.co.in');		
             /*
             if (args.format == "QR_CODE") {
                 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
